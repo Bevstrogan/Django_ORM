7f46da95375ca3dfefa2d0f2ae3a04b33ffc2e21
bevstrogan <bevstrogan@gmail.com>
2024-04-23 12:42:23 +0300

    2 дз после первой миграции

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_homework',
+        'USER': 'postgres',
+        'PASSWORD': '0835',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
